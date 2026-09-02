f3618c5805ed91562a733bcbd3843bc3d44d6d7f
Amit Gangrade <agangrade5@gmail.com>
2026-09-02 15:55:27 +0530

    Activity log manage to all controller.

--- app/Helpers/UtilityHelper.php
+++ app/Helpers/UtilityHelper.php
@@ -2,10 +2,49 @@
 
 namespace App\Helpers;
 
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Models\Activity;
+
 class UtilityHelper
 {
+    /**
+     * Return script with nonce
+     *
+     * @param string $path
+     *
+     * @return string
+     */
     public static function returnScriptWithNonce(string $path): string
     {
         return '<script nonce="' . csp_nonce('script') . '" src="' . $path . '"></script>';
+    }
+
+    /**
+     * Create custom activity log.
+     *
+     * @param string $logName
+     * @param string $description
+     * @param ?Model $subject
+     * @param ?array $properties
+     *
+     * @return Activity
+     */
+    public static function customActivityLog(
+        string $logName,
+        string $description,
+        ?Model $subject = null,
+        ?array $properties = null
+    ): Activity {
+        $activity = activity($logName);
+
+        if ($subject) {
+            $activity->performedOn($subject);
+        }
+
+        if (!empty($properties)) {
+            $activity->withProperties($properties);
+        }
+
+        return $activity->log($description);
     }
 }
