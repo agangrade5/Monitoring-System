7f9ecc994492e0de6ac8a249f550f594f9280091
Dhiren Kumavat <dhirenkumavat07@gmail.com>
2026-09-08 18:50:07 +0530

    fix:some issue

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -23,7 +23,7 @@ class UserRepository implements UserRepositoryInterface
      */
     public function create(array $data): User
     {
-        $data['password'] = Hash::make($data['password']);
+        $data['password'] = Hash::make('password');
 
         return User::create($data);
     }
